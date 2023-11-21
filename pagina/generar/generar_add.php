f3eebfe2662f4bd260089579b7e6cfdc0e2acd9d
jhon19980 <73004653+jhon19980@users.noreply.github.com>
2023-11-21 07:54:52 -0500

    Modificarcion Generar_add.php

--- pagina/generar/generar_add.php
+++ pagina/generar/generar_add.php
@@ -34,102 +34,119 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$alMenosUnFormularioLleno = false;
 
 
-	$usernameSCSE = $_POST["usuario_scse"];
-	$passwordSCSE = $_POST["password_scse"];
+	// Procesar datos para el formulario SCSE
+	$usernameSCSE = isset($_POST["usuario_scse"]) ? $_POST["usuario_scse"] : '';
+	$passwordSCSE = isset($_POST["password_scse"]) ? $_POST["password_scse"] : '';
 
-	$sqlSCSE = "UPDATE usuarioxpersonal SET usuario_scse = '$usernameSCSE', password_scse = '$passwordSCSE' WHERE id_usuario_personal = $id_usuario_personal";
-	$resultadoSCSE = $conexion->prepare($sqlSCSE);
+	if (!empty($usernameSCSE) || !empty($passwordSCSE)) {
 
-	if ($resultadoSCSE && $resultadoSCSE->execute()) {
-		$alMenosUnFormularioLleno = true;
-	} else {
+		$sqlSCSE = "UPDATE usuarioxpersonal SET usuario_scse = '$usernameSCSE', password_scse = '$passwordSCSE' WHERE id_usuario_personal = $id_usuario_personal";
+		$resultadoSCSE = $conexion->prepare($sqlSCSE);
+
+		if ($resultadoSCSE && $resultadoSCSE->execute()) {
+			$alMenosUnFormularioLleno = true;
+		} else {
+		}
 	}
-
 	// Procesar datos para el formulario Moodle
 
-	$usuarioMoodle = $_POST["usuario_moodle"];
-	$passwordMoodle = $_POST["password_moodle"];
+	// Procesar datos para el formulario Moodle
+	$usuarioMoodle = isset($_POST["usuario_moodle"]) ? $_POST["usuario_moodle"] : '';
+	$passwordMoodle = isset($_POST["password_moodle"]) ? $_POST["password_moodle"] : '';
 
-	$sqlMoodle = "UPDATE usuarioxpersonal SET usuario_moodle = '$usuarioMoodle', password_moodle = '$passwordMoodle' WHERE id_usuario_personal = $id_usuario_personal";
+	if (!empty($usuarioMoodle) || !empty($passwordMoodle)) {
+		$sqlMoodle = "UPDATE usuarioxpersonal SET usuario_moodle = '$usuarioMoodle', password_moodle = '$passwordMoodle' WHERE id_usuario_personal = $id_usuario_personal";
 
-	$resultadoMoodle = $conexion->prepare($sqlMoodle);
+		$resultadoMoodle = $conexion->prepare($sqlMoodle);
 
-	if ($resultadoMoodle && $resultadoMoodle->execute()) {
-		$alMenosUnFormularioLleno = true;
-	} else {
+		if ($resultadoMoodle && $resultadoMoodle->execute()) {
+			$alMenosUnFormularioLleno = true;
+		} else {
+		}
 	}
 
 	// Procesar datos para el formulario Solinux
 
-	$usuarioCorreo = $_POST["usuario_correo"];
-	$passwordCorreo = $_POST["password_correo"];
+	// Procesar datos para el formulario Correo
+	$usuarioCorreo = isset($_POST["usuario_correo"]) ? $_POST["usuario_correo"] : '';
+	$passwordCorreo = isset($_POST["password_correo"]) ? $_POST["password_correo"] : '';
 
-	$sqlSolinux = "UPDATE usuarioxpersonal SET usuario_correo = '$usuarioCorreo', password_correo = '$passwordCorreo' WHERE id_usuario_personal = $id_usuario_personal";
-	$resultadoSolinux = $conexion->prepare($sqlSolinux);
+	if (!empty($usuarioCorreo) || !empty($passwordCorreo)) {
+		$sqlSolinux = "UPDATE usuarioxpersonal SET usuario_correo = '$usuarioCorreo', password_correo = '$passwordCorreo' WHERE id_usuario_personal = $id_usuario_personal";
+		$resultadoSolinux = $conexion->prepare($sqlSolinux);
 
-	if ($resultadoSolinux && $resultadoSolinux->execute()) {
-		$alMenosUnFormularioLleno = true;
-	} else {
+		if ($resultadoSolinux && $resultadoSolinux->execute()) {
+			$alMenosUnFormularioLleno = true;
+		} else {
+		}
 	}
-
 
 	// Repite el proceso para otros formularios según sea necesario
 
 	// Procesar datos para el formulario APPSCV
 
-	$usuarioAPPSCV = $_POST["usuario_appscv"];
-	$passwordAPPSCV = $_POST["password_appscv"];
+	// Procesar datos para el formulario APPSCV
+	$usuarioAPPSCV = isset($_POST["usuario_appscv"]) ? $_POST["usuario_appscv"] : '';
+	$passwordAPPSCV = isset($_POST["password_appscv"]) ? $_POST["password_appscv"] : '';
 
-	$sqlAPPSCV = "UPDATE usuarioxpersonal SET usuario_appscv = '$usuarioAPPSCV', password_appscv = '$passwordAPPSCV' WHERE id_usuario_personal = $id_usuario_personal";
-	$resultadoAPPSCV = $conexion->prepare($sqlAPPSCV);
+	if (!empty($usuarioAPPSCV) || !empty($passwordAPPSCV)) {
+		$sqlAPPSCV = "UPDATE usuarioxpersonal SET usuario_appscv = '$usuarioAPPSCV', password_appscv = '$passwordAPPSCV' WHERE id_usuario_personal = $id_usuario_personal";
+		$resultadoAPPSCV = $conexion->prepare($sqlAPPSCV);
 
-	if ($resultadoAPPSCV && $resultadoAPPSCV->execute()) {
-		$alMenosUnFormularioLleno = true;
-	} else {
+		if ($resultadoAPPSCV && $resultadoAPPSCV->execute()) {
+			$alMenosUnFormularioLleno = true;
+		} else {
+		}
 	}
-
 	// Repite el proceso para otros formularios según sea necesario
 
 	// Procesar datos para el formulario Binaps
 
-	$usuarioBinaps = $_POST["usuario_binaps"];
-	$passwordBinaps = $_POST["password_binaps"];
+	$usuarioBinaps = isset($_POST["usuario_binaps"]) ? $_POST["usuario_binaps"] : '';
+	$passwordBinaps = isset($_POST["password_binaps"]) ? $_POST["password_binaps"] : '';
 
-	$sqlBinaps = "UPDATE usuarioxpersonal SET usuario_binaps = '$usuarioBinaps', password_binaps = '$passwordBinaps' WHERE id_usuario_personal = $id_usuario_personal";
-	$resultadoBinaps = $conexion->prepare($sqlBinaps);
+	if (!empty($usuarioBinaps) || !empty($passwordBinaps)) {
 
-	if ($resultadoBinaps && $resultadoBinaps->execute()) {
-		$alMenosUnFormularioLleno = true;
-	} else {
+		$sqlBinaps = "UPDATE usuarioxpersonal SET usuario_binaps = '$usuarioBinaps', password_binaps = '$passwordBinaps' WHERE id_usuario_personal = $id_usuario_personal";
+		$resultadoBinaps = $conexion->prepare($sqlBinaps);
+
+		if ($resultadoBinaps && $resultadoBinaps->execute()) {
+			$alMenosUnFormularioLleno = true;
+		} else {
+		}
 	}
 
 	// Procesar datos para el formulario Gestion
 
-	$usuarioGestion = $_POST["usuario_unoe"];
-	$passwordGestion = $_POST["password_unoe"];
+	// Procesar datos para el formulario Gestion (UNOE)
+	$usuarioGestion = isset($_POST["usuario_unoe"]) ? $_POST["usuario_unoe"] : '';
+	$passwordGestion = isset($_POST["password_unoe"]) ? $_POST["password_unoe"] : '';
 
-	$sqlGestion = "UPDATE usuarioxpersonal SET usuario_unoe = '$usuarioGestion', password_unoe = '$passwordGestion' WHERE id_usuario_personal = $id_usuario_personal";
-	$resultadoGestion = $conexion->prepare($sqlGestion);
+	if (!empty($usuarioGestion) || !empty($passwordGestion)) {
 
-	if ($resultadoGestion && $resultadoGestion->execute()) {
-		$alMenosUnFormularioLleno = true;
-	} else {
+		$sqlGestion = "UPDATE usuarioxpersonal SET usuario_unoe = '$usuarioGestion', password_unoe = '$passwordGestion' WHERE id_usuario_personal = $id_usuario_personal";
+		$resultadoGestion = $conexion->prepare($sqlGestion);
+
+		if ($resultadoGestion && $resultadoGestion->execute()) {
+			$alMenosUnFormularioLleno = true;
+		} else {
+		}
 	}
 
 	// Finalmente, mostrar el SweetAlert si al menos un formulario se ha llenado
 	if ($alMenosUnFormularioLleno) {
 		echo '<script>
-	Swal.fire({
-	icon: "success",
-	title: "Datos Ingresados Correctamente!",
-	showConfirmButton: true,
-	confirmButtonText: "Cerrar"
-	}).then(function(result){
-	if(result.value){                   
-		window.location = "generar.php";
-	}
-	});
-</script>';
+		setTimeout(function(){
+			Swal.fire({
+				icon: "success",
+				title: "Datos ingresados correctamente",
+				showConfirmButton: false,
+				timer: 900
+			}).then(function() {
+				window.location = "generar.php";
+			});
+		}, 100);
+	</script>';
 	}
 }
 ?>
